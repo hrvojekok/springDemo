be502f17a166433dfedab449b057fc6c47560ede
Hrvoje Kokosarevic <hrvoje.kok@gmail.com>
2020-06-26 12:25:00 +0200

    update

--- demo/src/main/java/com/example/demo/model/Person.java
+++ demo/src/main/java/com/example/demo/model/Person.java
@@ -1,6 +1,8 @@
 package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.lang.NonNull;
+
 
 import java.util.UUID;
 
@@ -8,12 +10,14 @@ public class Person {
 
     private final UUID id;
 
+    @NonNull
+    private final String name;
+
     public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name) {
         this.id = id;
         this.name = name;
     }
 
-    private final String name;
 
     public UUID getId() {
         return id;
